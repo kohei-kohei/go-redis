9b15dfe436530ceebf10c378799247dd6b52fe62
kohei-kohei <sky6sk212@gmail.com>
2022-07-31 16:51:22 +0900

    convert domain struct

--- api/db/db.go
+++ api/db/db.go
@@ -6,28 +6,32 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
+	"github.com/kohei-kohei/go-redis/domain"
 )
 
-type Bread struct {
+type bread struct {
 	ID        int       `db:"id"`
 	Name      string    `db:"name"`
 	CreatedAt time.Time `db:"created_at"`
 }
 
-func GetBread(id int) (*Bread, error) {
+func GetBread(id int) (*domain.Bread, error) {
 	db, err := sqlx.Open("mysql", "go_test:password@tcp(db:3306)/go_database?parseTime=true")
 	defer db.Close()
 	if err != nil {
 		return nil, err
 	}
 
-	var bread Bread
-	err = db.Get(&bread, "SELECT * FROM bread WHERE id = ?", id)
+	var b bread
+	err = db.Get(&b, "SELECT * FROM bread WHERE id = ?", id)
 	if err == sql.ErrNoRows {
 		return nil, nil
 	} else if err != nil {
 		return nil, err
 	}
 
-	return &bread, nil
+	return &domain.Bread{
+		ID:   b.ID,
+		Name: b.Name,
+	}, nil
 }
---
+++ api/domain/bread.go
@@ -0,0 +1,6 @@
+package domain
+
+type Bread struct {
+	ID   int
+	Name string
+}
